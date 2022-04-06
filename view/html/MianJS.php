b104c4aaef26cff6233dd1f2862af5b18f0660e6
Norman13xx5 <brahyan.com@gmail.com>
2022-04-06 15:03:01 -0500

    main

---
+++ view/html/MianJS.php
@@ -0,0 +1,9 @@
+<script src="../../public/lib/jquery/jquery.js"></script>
+<script src="../../public/lib/popper.js/popper.js"></script>
+<script src="../../public/lib/bootstrap/bootstrap.js"></script>
+<script src="../../public/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js"></script>
+<script src="../../public/lib/moment/moment.js"></script>
+<script src="../../public/lib/jquery-ui/jquery-ui.js"></script>
+<script src="../../public/lib/jquery-switchbutton/jquery.switchButton.js"></script>
+<script src="../../public/lib/peity/jquery.peity.js"></script>
+<script src="../../public/js/bracket.js"></script>
