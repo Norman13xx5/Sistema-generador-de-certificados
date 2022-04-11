a56cb72db3b12a97a475e59c6ed02901a6a3519b
Norman13xx5 <brahyan.com@gmail.com>
2022-04-11 00:25:12 -0500

    main

--- view/Certificado/certificado.js
+++ view/Certificado/certificado.js
@@ -1,10 +1,19 @@
+const myCanvas = document.getElementById('myCanvas');
+const ctx = myCanvas.getContext('2d');
+
+const image = new Image();
+image.src = '../../public/img/certificado.png';
+ctx.drawImage(image, 0, 0, myCanvas.width, myCanvas.height);
+
 $(document).ready(function () {
     var id_curso_detalle = getUrlParameter('id_curso_detalle');
     $.post("../../controller/usuario.php?op=mostrar_curso_datellate", { id_curso_detalle: id_curso_detalle }, function (data) {
         data = JSON.parse(data);
-        console.log(data);
+        /*  console.log(data); */
         $('#descripcion_curso').html(data.descripcion_curso);
     });
+
+
 });
 
 var getUrlParameter = function getUrlParameter(sParam) {
--- view/Certificado/index.php
+++ view/Certificado/index.php
@@ -11,7 +11,10 @@
 
     <div class="ht-100v d-flex align-items-center justify-content-center">
         <div class="wd-lg-70p wd-xl-50p tx-center pd-x-40">
-            <h1 class="tx-100 tx-xs-140 tx-normal tx-inverse tx-roboto mg-b-0"><img src="../../public/img/certificado.png" class="img-fluid" alt="Responsive Image"></h1>
+            <h1 class="tx-100 tx-xs-140 tx-normal tx-inverse tx-roboto mg-b-0">
+                <canvas id="myCanvas" height="100px" width="100px" class="img-fluid" alt="Responsive image"></canvas>
+                <!-- <img src="../../public/img/certificado.png" > -->
+            </h1>
             <br></br>
             <p class="tx-16 mg-b-30 text-justify" id="descripcion_curso"></p>
             <div class="form-layout-footer">
