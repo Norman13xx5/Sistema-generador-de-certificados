25233e701e8ffe84104c0530b0121af85b198408
Norman13xx5 <brahyan.com@gmail.com>
2022-04-10 21:37:10 -0500

    main

--- models/Usuario.php
+++ models/Usuario.php
@@ -36,4 +36,34 @@ class Usuario extends Conectar
             }
         }
     }
+    /* Mostrar todos los cursos en los cuales está inscrito un usuario */
+    public function get_cursos_x_usuario($id_usuario)
+    {
+        $conectar = parent::conexion();
+        parent::set_name();
+        $sql = "SELECT 
+        td_curso_usuario.id_curso_detalle,
+        tm_curso.id_curso,
+        tm_curso.nombre_curso,
+        tm_curso.descripcion_curso,
+        tm_curso.fecha_inicio_curso,
+        tm_curso.fecha_final_curso,
+        tm_usuario.id_usuario,
+        tm_usuario.nombre_usuario,
+        tm_usuario.apellido_paterno,
+        tm_usuario.apellido_materno,
+        tm_instructor.id_instrutor,
+        tm_instructor.instrutor_nombre,
+        tm_instructor.apellido_paterno,
+        tm_instructor.apellido_materno
+        FROM td_curso_usuario INNER JOIN 
+        tm_curso ON td_curso_usuario.id_curso = tm_curso.id_curso INNER JOIN
+        tm_usuario ON td_curso_usuario.id_usuario = tm_usuario.id_usuario INNER JOIN
+        tm_instructor ON tm_curso.id_instrutor =tm_instructor.id_instrutor
+        WHERE td_curso_usuario.id_usuario = ?";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $id_usuario);
+        $sql->execute();
+        return $resultado=$sql->fetchAll();
+    }
 }
