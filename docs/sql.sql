281ca4e6e91e9679f0289681944f12c0cef3a62f
Norman13xx5 <brahyan.com@gmail.com>
2022-04-10 21:25:32 -0500

    main

---
+++ docs/sql.sql
@@ -0,0 +1,20 @@
+SELECT 
+td_curso_usuario.id_curso_detalle,
+tm_curso.id_curso,
+tm_curso.nombre_curso,
+tm_curso.descripcion_curso,
+tm_curso.fecha_inicio_curso,
+tm_curso.fecha_final_curso,
+tm_usuario.id_usuario,
+tm_usuario.nombre_usuario,
+tm_usuario.apellido_paterno,
+tm_usuario.apellido_materno,
+tm_instructor.id_instrutor,
+tm_instructor.instrutor_nombre,
+tm_instructor.apellido_paterno,
+tm_instructor.apellido_materno
+FROM td_curso_usuario INNER JOIN 
+tm_curso ON td_curso_usuario.id_curso = tm_curso.id_curso INNER JOIN
+tm_usuario ON td_curso_usuario.id_usuario = tm_usuario.id_usuario INNER JOIN
+tm_instructor ON tm_curso.id_instrutor =tm_instructor.id_instrutor
+WHERE td_curso_usuario.id_usuario = 1
