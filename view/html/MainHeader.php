b4bc01a7b4aa5eca39d82f77e507f7dd0ae6e3e6
Norman13xx5 <brahyan.com@gmail.com>
2022-04-10 21:00:29 -0500

    main

--- view/html/MainHeader.php
+++ view/html/MainHeader.php
@@ -7,10 +7,12 @@
         <nav class="nav">
             <div class="dropdown">
                 <a href="" class="nav-link nav-link-profile" data-toggle="dropdown">
-                    <span class="logged-name hidden-md-down"><?php echo $_SESSION["nombre_usuario"]?></span>
+                    <span class="logged-name hidden-md-down"><?php echo $_SESSION["nombre_usuario"] ?></span>
                     <img src="http://via.placeholder.com/64x64" class="wd-32 rounded-circle" alt="">
                     <span class="square-10 bg-success"></span>
                 </a>
+                <!-- id del usuario -->
+                <input type="hidden" id="id_usuario" value="<?php echo $_SESSION["id_usuario"] ?>">
                 <div class="dropdown-menu dropdown-menu-header wd-200">
                     <ul class="list-unstyled user-profile-nav">
                         <li><a href="../UsuPerfil/index.php"><i class="icon ion-ios-gear"></i> Perfil</a></li>
