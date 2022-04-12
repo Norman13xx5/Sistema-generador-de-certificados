d5436180acb414bdefb67c195d10a4f9a2a713ca
Norman13xx5 <brahyan.com@gmail.com>
2022-04-12 01:27:10 -0500

    MAIN

--- view/Certificado/certificado.js
+++ view/Certificado/certificado.js
@@ -1,9 +1,32 @@
 const myCanvas = document.getElementById('myCanvas');
 const ctx = myCanvas.getContext('2d');
 
+/* Inicializamos la imagen */
 const image = new Image();
+/* Ruta de la imgane */
 image.src = '../../public/img/certificado.png';
+/* Dimensionamos y seleccionamos imagen */
 ctx.drawImage(image, 0, 0, myCanvas.width, myCanvas.height);
+/* Tamaño de fuente */
+ctx.font = '40px Sanchez';
+ctx.font = 'N';
+ctx.textAling = 'center';
+ctx.textBaseline = 'middle';
+/* Moverli verticalmente de izquierda a derecha */
+var x = myCanvas.width / 2.93;
+/* Nombre de alumno */
+ctx.fillText("BRAYAN DIAZ MARTINEZ", x, 333)
+
+/* Nombre del certificado */
+ctx.font = '17px Sanchez';
+ctx.fillText("Por su rendimiento ejemplar en el curso de programación php.", x, 390)
+
+
+/* Nombre del instructor */
+var x = myCanvas.width / 20;
+ctx.font = '17px League Sparta  ';
+ctx.fillText("BRAYAN DIAZ MARTINEZ.", x, 480)
+
 
 $(document).ready(function () {
     var id_curso_detalle = getUrlParameter('id_curso_detalle');
